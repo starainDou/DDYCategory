2472e350de73e9b7e0470f62c4f0a7299888e254
rain <634778311@qq.com>
2019-08-22 14:31:25 +0800

    更新模型数组排序

--- DDYCategory/DDYCategory/DDYCategory/Foundation/NSArray+DDYExtension.h
+++ DDYCategory/DDYCategory/DDYCategory/Foundation/NSArray+DDYExtension.h
@@ -23,4 +23,13 @@
  */
 - (NSMutableArray *)ddy_SortWithModel:(NSString *)model selector:(SEL)selector showSearch:(BOOL)show;
 
+/**
+ 对模型数组进行索引排序(直接block回调排序后数组和标题数组)
+ @param selector 模型排序依据属性
+ @param complete 完成后回调(modelsArray:排序后模型数组，titlesArray：标题数组)
+
+ */
+- (void)ddy_ModelSortSelector:(SEL)selector complete:(void (^)(NSArray *modelsArray, NSArray *titlesArray))complete;
+
+
 @end
--- DDYCategory/DDYCategory/DDYCategory/Foundation/NSArray+DDYExtension.m
+++ DDYCategory/DDYCategory/DDYCategory/Foundation/NSArray+DDYExtension.m
@@ -73,4 +73,41 @@
     return NO;
 }
 
+#pragma mark  对模型数组进行索引排序
+- (void)ddy_ModelSortSelector:(SEL)selector complete:(void (^)(NSArray *modelsArray, NSArray *titlesArray))complete {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        // 索引规则对象
+        UILocalizedIndexedCollation *collation = [UILocalizedIndexedCollation currentCollation];
+        // 索引数量（26个字母和1个#）
+        NSInteger sectionTitlesCount = collation.sectionTitles.count;
+        // 临时存储分组的数组
+        NSMutableArray *tempArray = [[NSMutableArray alloc] initWithCapacity:sectionTitlesCount];
+        // 最终去空的分组数组
+        NSMutableArray *sortedModelArray = [NSMutableArray arrayWithCapacity:sectionTitlesCount];
+        // 最终储存标题的数组
+        NSMutableArray *sectionTitleArray = [NSMutableArray arrayWithCapacity:sectionTitlesCount];
+        // @[ @[A], @[B], @[C] ... @[Z], @[#] ] 27个空数组(@[ @[], @[], @[] ... @[], @[] ])
+        for (int i = 0; i < sectionTitlesCount; i++) {
+            [tempArray addObject:[NSMutableArray array]];
+        }
+        // 按selector参数分配到数组
+        for (id obj in self) {
+            NSInteger sectionNumber = [collation sectionForObject:obj collationStringSelector:selector];
+            [[tempArray objectAtIndex:sectionNumber] addObject:obj];
+        }
+        // 对每个数组按排序 同时去除空数组
+        for (int i = 0; i < sectionTitlesCount; i++) {
+            if (tempArray[i] && [tempArray[i] count]) {
+                [sortedModelArray addObject:[collation sortedArrayFromArray:tempArray[i] collationStringSelector:selector]];
+                [sectionTitleArray addObject:[UILocalizedIndexedCollation currentCollation].sectionTitles[i]];
+            }
+        }
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (complete) {
+                complete(sortedModelArray, sectionTitleArray);
+            }
+        });
+    });
+}
+
 @end
